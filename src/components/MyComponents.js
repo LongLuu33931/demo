f83925bdec8dc9ed4fa71d1dc67e9170b02f9571
LongLuu <64773511+LongLuu33931@users.noreply.github.com>
2022-11-03 18:57:54 +0700

    my state

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Long Lưu",
+    address: "HCM",
+    age: 21,
+  };
   render() {
-    return <div>My First Component {Math.random()}</div>;
+    return (
+      <div>
+        <div>My Name is: {this.state.name}</div>
+        <div>My Address is: {this.state.address}</div>
+        <div>My Age is: {this.state.age}</div>
+      </div>
+    );
   }
 }
 
