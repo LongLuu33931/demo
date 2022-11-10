00087cebb1860d2d882bc2bbe716c0e4f077b015
LongLuu <64773511+LongLuu33931@users.noreply.github.com>
2022-11-10 17:57:33 +0700

    DOM Events

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,12 +6,16 @@ class MyComponent extends React.Component {
     address: "HCM",
     age: 21,
   };
+  handleClick() {
+    console.log("hehe");
+  }
   render() {
     return (
       <div>
         <div>My Name is: {this.state.name}</div>
         <div>My Address is: {this.state.address}</div>
         <div>My Age is: {this.state.age}</div>
+        <button onClick={this.handleClick}>Click me!</button>
       </div>
     );
   }
